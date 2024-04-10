182d613031b42756f50a7019a966a655c8765ba9
skiller70 <singhlucky007007@gmail.com>
2024-04-10 12:08:33 +0530

    initial commit

---
+++ sanity/user.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'heros',
+    title: 'Heros',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Add Hero',
+        type: 'string',
+      },
+    
+      {
+        title: 'Launch Scheduled At',
+        name: 'launchAt',
+        type: 'datetime'
+      },
+      
+    ],
+  };
